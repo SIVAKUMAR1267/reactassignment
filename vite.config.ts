3b8561a7e5fff1890370d268b15b108ea3ec2ae5
SIVAKUMAR1267 <sivakumar.r.6324@gmail.com>
2025-07-31 20:56:08 +0530

    third commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+          plugins: [react()],
+          server: {
+            port: 3000, // Port for development server
+          },
+          preview: {
+            port: 8080, // Port for serving the production build
+          }
+        })
